7537e7f4c2409166e38cc6c3eea13da964445e00
N-meapp <developersnme@gmail.com>
2025-09-26 09:37:11 +0530

    updated

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -15,8 +15,11 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default="postgres://user:password@hostname:port/dbname"
+    )
 }
+
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost']
 
